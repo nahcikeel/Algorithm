cd77bb220f19d780e1fb94a261ff67910a8ea42c
leekichan <73771203+nahcikeel@users.noreply.github.com>
2023-09-04 11:19:49 +0900

    설명 추가

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,9 +1,9 @@
 def solution(arr):
-    answer = []
-    answer.append(arr[0])
+    answer = []                        # 정답이 들어갈 리스트
+    answer.append(arr[0])              # 0번째 숫자는 무조건 포함
     
-    for i in range(1,len(arr)):
-        if arr[i] != arr[i-1]:
-            answer.append(arr[i])
+    for i in range(1,len(arr)):        # 1번째 숫자부터 마지막 숫자까지 반복
+        if arr[i] != arr[i-1]:         # 이전의 숫자와 다르다면 
+            answer.append(arr[i])      # 정답 리스트에 추가
             
     return answer
