87af29e872a5ed118a8d6fc3dbed0352aa220a65
leekichan <73771203+nahcikeel@users.noreply.github.com>
2023-10-09 16:17:11 +0900

    Update 먹을 것인가 먹힐 것인가.py

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -2,13 +2,11 @@ import sys
 
 N = int(input())
 
-for _ in range(N):
+for _ in range(N):    # 반복횟수 인풋
     
-    s,t = map(int, sys.stdin.readline().split())
-    a = list(map(int, sys.stdin.readline().split()))
+    s,t = map(int, sys.stdin.readline().split())     
+    a = list(map(int, sys.stdin.readline().split()))    
     b = list(map(int, sys.stdin.readline().split()))
-
-    cnt = 0
 
     a.sort()
     b.sort()
@@ -16,13 +14,13 @@ for _ in range(N):
     cnt = 0
     ans = 0
 
-    for i in range(s):
+    for i in range(s):    # b의 길이만큼 반복
         while True:
-             if cnt == t or a[i] <= b[cnt]:
-                ans += cnt
+             if cnt == t or a[i] <= b[cnt]:    # a의 원소가 b의 원소보다 작아지거나, t의 원소를 다 돌면
+                ans += cnt    # answer에 cnt 더해주면서 탈출
                 break
              else:
-                cnt += 1
+                cnt += 1    # 아니라면 +1씩 증가
        
     print(ans)
 
