5b1347189b3879296d7af1ff893c01ccb187b8f1
leekichan <73771203+nahcikeel@users.noreply.github.com>
2023-10-09 16:28:59 +0900

    Update 먹을 것인가 먹힐 것인가.py

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -11,16 +11,16 @@ for _ in range(N):    # 반복횟수 인풋
     a.sort()
     b.sort()
 
-    cnt = 0
-    ans = 0
+    cnt = 0    # 'a의 원소 > b의 원소' 개수를 셀 변수
+    ans = 0    # 누적된 정답을 담을 변수
 
-    for i in range(s):    # b의 길이만큼 반복
+    for i in range(s):    # a의 길이만큼 반복
         while True:
-             if cnt == t or a[i] <= b[cnt]:    # a의 원소가 b의 원소보다 작아지거나, t의 원소를 다 돌면
-                ans += cnt    # answer에 cnt 더해주면서 탈출
+             if cnt == t or a[i] <= b[cnt]:    # t의 원소를 다 돌거나, a의 원소가 b의 원소보다 작아지면
+                ans += cnt    # ans에 cnt 더해주면서 종료 => 다음 a원소 시작
                 break
              else:
-                cnt += 1    # 아니라면 +1씩 증가
+                cnt += 1    # 아니라면 +1씩 증가(a의 원소 > b의 원소)  
        
     print(ans)
 
