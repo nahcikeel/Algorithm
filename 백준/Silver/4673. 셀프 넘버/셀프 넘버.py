aa7ec5cb62990832d1bf534f8a93f50bad2a65c5
leekichan <73771203+nahcikeel@users.noreply.github.com>
2023-11-10 14:25:04 +0900

    Update 셀프 넘버.py

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,18 +1,19 @@
-Self = list(range(1,10001))
+# notSelf : 생성자 있는 숫자
+
+ALL = list(range(1,10001))  # 전체 1~10000 
 notSelf = []
-a = 0
 
 for i in range(1,10001):
-    for j in str(i):
-        i += int(j)
+    for j in str(i):    
+        i += int(j)        # 전체숫자+자릿수 더해주기
     if i <= 10000:
-            notSelf.append(i)
+            notSelf.append(i)    # 10000이하면 리스트에 추가
 
-notSelf = list(set(notSelf))
+notSelf = list(set(notSelf))     # set으로 중복 제거하고 다시 set에 넣기 
 
 for m in notSelf:
-    if m in Self:
-        Self.remove(m)
+    if m in ALL:
+        ALL.remove(m)  # 1~10000 중에 겹치는 숫자 제거
 
-for n in Self:
+for n in ALL:
     print(n)
