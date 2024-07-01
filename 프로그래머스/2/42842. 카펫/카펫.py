3c89d54aca86cc068c73f96be7e8bb0a9213c985
Kichan Lee <73771203+nahcikeel@users.noreply.github.com>
2024-07-01 17:39:09 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,22 +1,11 @@
 def solution(brown, yellow):
-    answer = []
-    square = brown+yellow
-    
-    for i in range(3,square):
-        if square%i==0:
-            answer.append([square//i, i])
-            
-    for sq in answer:
-        wide = sq[0]
-        height = sq[1]
-        
-        if wide>=height:
-        
-            con1 = (wide-2)*(height-2)
-            con2 = 2*wide + 2*height -4
 
-            if (con1==yellow) & (con2==brown):
-                ans = [wide,height]
+    square = brown+yellow  # 총면적
     
+    for height in range(3,square):
+        if square%height==0:
+            width = square//height
+            
+            if (width-2)*(height-2) == yellow:
+                return [width,height]
     
-    return ans
