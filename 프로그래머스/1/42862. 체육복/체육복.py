ec4d3d11cd4e57b0e4b8e00c2aad3aff1717de90
Kichan Lee <73771203+nahcikeel@users.noreply.github.com>
2024-07-16 15:53:12 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,29 +1,27 @@
 def solution(n, lost, reserve):
-    # 각 학생이 체육복을 가지고 있는 상태를 표시하는 리스트
-    st = [1] * n
+    answer = 0
     
-    # 체육복을 도난당한 학생들에 대해 상태를 업데이트
+    students = [1] * n
+    
     for i in lost:
-        st[i - 1] -= 1
+        students[i-1] -= 1
+    for j in reserve:
+        students[j-1] += 1
+        
+    for k in range(n):
+        if students[k]==0:
+            if k > 0 and students[k-1] > 1:
+                students[k] += 1
+                students[k-1] -= 1
+                
+            elif k < n-1 and students[k+1] > 1:
+                students[k] += 1
+                students[k+1] -= 1
+                
+            
     
-    # 여분의 체육복을 가진 학생들에 대해 상태를 업데이트
-    for i in reserve:
-        st[i - 1] += 1
-    
-    # 체육복을 빌려주는 과정
-    for i in range(n):
-        if st[i] == 0:
-            if i > 0 and st[i - 1] > 1:
-                st[i] += 1
-                st[i - 1] -= 1
-            elif i < n - 1 and st[i + 1] > 1:
-                st[i] += 1
-                st[i + 1] -= 1
-    
-    # 체육 수업을 들을 수 있는 학생 수 계산
-    answer = sum(1 for x in st if x > 0)
+    for x in students:
+        if x >0:
+            answer += 1
     
     return answer
-
-# 예제 실행
-print(solution(5, [2, 4], [3]))  # 예시: 5명의 학생 중 2번과 4번 학생이 잃어버렸고, 3번 학생이 여분을 가지고 있는 경우
