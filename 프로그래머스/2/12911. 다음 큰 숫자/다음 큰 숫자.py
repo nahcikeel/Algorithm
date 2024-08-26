033b6875adc87e25e5eace6a4559982c85497569
Kichan Lee <73771203+nahcikeel@users.noreply.github.com>
2024-08-26 10:05:56 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 9.99 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -0,0 +1,11 @@
+def solution(n):
+    
+    answer = (n+1)
+    
+    while True:
+        if bin(n).count('1') == bin(answer).count('1'):
+            break
+    
+        answer += 1
+    
+    return answer
