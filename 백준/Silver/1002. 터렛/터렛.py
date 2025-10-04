ef2cfa28305e52b689534da480ab9a16110cbe6a
Kichan Lee <73771203+nahcikeel@users.noreply.github.com>
2025-10-04 09:58:17 +0900

    [Silver III] Title: 터렛, Time: 40 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Silver/1002. 터렛/터렛.py
@@ -0,0 +1,18 @@
+import math
+
+t = int(input())
+for _ in range(t):
+    x1,y1,r1,x2,y2,r2 = map(int, input().split())
+    d = math.sqrt((x1-x2)**2 + (y1-y2)**2)
+    
+    if x1==x2 and y1==y2 and r1==r2:
+        print(-1)
+
+    elif d==r1+r2 or d==abs(r1-r2):
+        print(1)
+
+    elif d<abs(r1-r2) or d>r1+r2:
+        print(0)
+
+    else:
+        print(2)
