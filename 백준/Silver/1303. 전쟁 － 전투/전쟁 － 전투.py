526b5278c3d71cf7118711cc5584d5ac55701a80
Kichan Lee <73771203+nahcikeel@users.noreply.github.com>
2025-10-16 00:44:07 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 64 ms, Memory: 34968 KB -BaekjoonHub

---
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+n, m = map(int, input().split())  # n: 가로, m: 세로
+war = [list(input().strip()) for _ in range(m)]
+visited = [[False] * n for _ in range(m)]
+
+dirs = [(0,1), (0,-1), (1,0), (-1,0)]
+
+def bfs(y, x, team):
+    q = deque([(y, x)])
+    visited[y][x] = True
+    cnt = 1
+
+    while q:
+        cy, cx = q.popleft()
+
+        for dy, dx in dirs:
+            ny = cy + dy
+            nx = cx + dx
+
+            if 0 <= ny < m and 0 <= nx < n and not visited[ny][nx] and war[ny][nx] == team:
+                visited[ny][nx] = True
+                q.append((ny, nx))
+                cnt += 1
+
+    return cnt
+
+white = 0
+blue = 0
+
+for y in range(m):
+    for x in range(n):
+        if not visited[y][x]:
+            if war[y][x] == 'W':
+                white += bfs(y, x, 'W') ** 2
+            else:
+                blue += bfs(y, x, 'B') ** 2
+
+print(white, blue)
