7eaedcfd178fa3f9b04e60105ff128e92f3cdc03
Kichan Lee <73771203+nahcikeel@users.noreply.github.com>
2025-12-09 11:31:53 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 56 ms, Memory: 34900 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+N, K = map(int, input().split())
+q = deque(range(1, N+1))
+
+result = []
+
+while q:
+    q.rotate(-(K-1))
+    result.append(q.popleft())
+
+print("<" + ", ".join(map(str, result)) + ">")
