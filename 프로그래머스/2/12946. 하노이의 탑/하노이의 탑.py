88a081b0b2b735f1407ef9dea7a960e34f19e1e0
Kichan Lee <73771203+nahcikeel@users.noreply.github.com>
2026-02-04 09:43:17 +0900

    [level 2] Title: 하노이의 탑, Time: 9.15 ms, Memory: 17.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = []
+    
+    def hanoi(n,start,mid,end):
+        if n == 1:
+            answer.append([start,end])
+            return
+        hanoi(n-1, start, end, mid)
+        answer.append([start,end])
+        hanoi(n-1, mid,start, end)
+    hanoi(n,1,2,3)
+    return answer
