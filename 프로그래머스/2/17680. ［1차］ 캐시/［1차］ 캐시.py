a0a79f90873f57aae5450a3dc0dbe6b2df74396f
Kichan Lee <73771203+nahcikeel@users.noreply.github.com>
2026-02-12 11:02:47 +0900

    [level 2] Title: [1차] 캐시, Time: 68.70 ms, Memory: 16.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -0,0 +1,24 @@
+def solution(cacheSize, cities):
+    
+    if cacheSize == 0:
+        return len(cities)*5
+    
+    cache = []
+    time = 0
+    
+    for city in cities:
+        
+        city = city.lower()
+        
+        if city in cache:
+            cache.remove(city)
+            cache.append(city)
+            time += 1
+            
+        else:
+            if len(cache) >= cacheSize:
+                cache.pop(0)
+            cache.append(city)
+            time += 5
+    
+    return time
