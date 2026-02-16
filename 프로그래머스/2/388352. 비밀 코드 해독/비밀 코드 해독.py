7e68b8a5e9f83b808f4ded8c9723cce7b251ff80
Kichan Lee <73771203+nahcikeel@users.noreply.github.com>
2026-02-16 09:14:03 +0900

    [level 2] Title: 비밀 코드 해독, Time: 210.16 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+
+def solution(n, q, ans):
+    answer = 0
+    
+    for group in combinations(range(1,n+1),5):
+        valid = True
+        
+        for i in range(len(q)):
+            if len(set(q[i]) & set(group)) != ans[i]:
+                valid = False
+                break
+
+        if valid:
+            answer += 1
+    
+    return answer
