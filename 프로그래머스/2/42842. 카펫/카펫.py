8cbe7a9fc9c75e728d7ebab2a1794fe5e8ee789f
Kichan Lee <73771203+nahcikeel@users.noreply.github.com>
2026-03-02 17:00:52 +0900

    [level 2] Title: 카펫, Time: 0.70 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,11 +1,17 @@
 def solution(brown, yellow):
-
-    square = brown+yellow  # 총면적
     
-    for height in range(3,int(square**0.5) + 1):
-        if square%height==0:
-            width = square//height
+    width  = 0  # 가로
+    height = 0  # 세로
+    dimension = brown + yellow  # 넓이
+
+    
+    for i in range(3,int(dimension**0.5)+1):
+        if dimension % i == 0:
+            width  = max(dimension//i, i)
+            height = min(dimension//i, i)
             
-            if (width-2)*(height-2) == yellow:
+            if width*2 + (height-2)*2 == brown:
                 return [width,height]
+            
+    
     
