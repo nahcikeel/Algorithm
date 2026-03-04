5902cc9d4d8fe87b170c5db2cd9d4acc6f12fecc
Kichan Lee <73771203+nahcikeel@users.noreply.github.com>
2026-03-04 10:05:39 +0900

    [level 3] Title: 스티커 모으기(2), Time: 57.61 ms, Memory: 15.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -0,0 +1,23 @@
+def solution(sticker):
+
+    n = len(sticker)
+    if n==1:
+        return sticker[0]
+    
+    # 첫번째 사용 - 마지막 미사용
+    dp1 = [0] * n
+    dp1[0] = sticker[0]
+    dp1[1] = max(sticker[0], sticker[1])
+    
+    for i in range(2,n-1):
+        dp1[i] = max(dp1[i-2]+sticker[i], dp1[i-1])
+        
+    # 첫번째 미사용 - 마지막 사용
+    dp2 = [0] * n
+    dp2[1] = sticker[1]
+    
+    for i in range(2,n):
+        dp2[i] = max(dp2[i-2]+sticker[i], dp2[i-1])
+
+    
+    return max(dp1[n-2], dp2[n-1])
