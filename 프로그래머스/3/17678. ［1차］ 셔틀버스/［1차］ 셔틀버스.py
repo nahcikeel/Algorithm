32ae7e49b50b31c2dd726f1707341acc85fbe2d0
Kichan Lee <73771203+nahcikeel@users.noreply.github.com>
2026-03-08 14:27:54 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 1.16 ms, Memory: 9.6 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
@@ -1,6 +1,6 @@
 def HtoM(time1):
     h,m = map(int, time1.split(':'))
-    return h*60+m
+    return h*60 + m
 
 def MtoH(time2):
     h = time2//60
@@ -9,25 +9,26 @@ def MtoH(time2):
 
 def solution(n, t, m, timetable):
     
-    # 사람들 시간변환
     timetable = [HtoM(t) for t in timetable]
     timetable.sort()
     
-    # 버스 시간변환
     bus1 = HtoM('09:00')
     buses = [bus1 + t*i for i in range(n)]
     
-    # 처리
     idx = 0
     for bus in buses:
         cnt = 0
-        
-        while idx < len(timetable) and cnt < m and bus >= timetable[idx]:
-            cnt += 1
+        while idx < len(timetable) and timetable[idx] <= bus and cnt < m:
             idx += 1
+            cnt += 1
     
     last_bus = buses[-1]
+    
+    # 마지막 버스에 자리가 남았다면
     if cnt < m:
         return MtoH(last_bus)
     
-    return MtoH(timetable[idx-1]-1)
+    # 마지막 버스에도 자리가 없으면
+    # idx -> 마지막 버스에 탑승한 사람을 가리키는 중
+    else:
+        return MtoH(timetable[idx-1]-1)
